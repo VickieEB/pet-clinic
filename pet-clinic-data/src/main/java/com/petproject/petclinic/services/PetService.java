904e8a376951f34e1ba364502aa28e763d53857a
VickieEB <victoria.etim.bassey@gmail.com>
2020-04-23 23:08:20 +0100

    Create Interfaces for Services Closes #5

---
+++ pet-clinic-data/src/main/java/com/petproject/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.petproject.petclinic.services;
+
+import com.petproject.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
