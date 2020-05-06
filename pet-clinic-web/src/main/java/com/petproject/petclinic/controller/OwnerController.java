bf2fa0118ba6f0b60124a499a3ddbee636b49446
VickieEB <victoria.etim.bassey@gmail.com>
2020-05-06 08:47:50 +0100

    Thymeleaf Exception While viewing Owner List.

--- pet-clinic-web/src/main/java/com/petproject/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/petproject/petclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package com.petproject.petclinic.controller;
 
+import com.petproject.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    public final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listPetOwners(){
+    public String listPetOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
