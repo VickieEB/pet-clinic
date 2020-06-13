fa3015d086c539ee615bb2cf753ea3fb53f5c137
VickieEB <victoria.etim.bassey@gmail.com>
2020-06-14 00:44:04 +0100

    Add Maven Surefire Plugin. Closes #4

--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,15 +1,18 @@
 package petclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@SpringBootApplication
 class PetClinicApplicationTests {
 
-
     @Test
-    void testSomething(){
+    void contextLoads(){
 
     }
 
