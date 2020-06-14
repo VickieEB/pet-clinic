0345f36d6d72fe40345e54e6237bbdb8de6b6001
VickieEB <victoria.etim.bassey@gmail.com>
2020-06-14 22:17:10 +0100

    Fix Application Test package. Closes #4

--- pet-clinic-web/src/test/java/com/petproject/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/petproject/petclinic/PetClinicApplicationTests.java
@@ -1,14 +1,12 @@
-package petclinic;
+package com.petproject.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@SpringBootApplication
 class PetClinicApplicationTests {
 
     @Test
